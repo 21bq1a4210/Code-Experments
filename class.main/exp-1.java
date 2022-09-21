8ccab3867e36e62bff801a7d95f946fa64ac3d8d
21bq1a4210 <113082987+21bq1a4210@users.noreply.github.com>
2022-09-21 19:50:11 +0530

    Update exp-1.java
    added comments

--- class.main/exp-1.java
+++ class.main/exp-1.java
@@ -1,17 +1,19 @@
 import javax.sound.sampled.SourceDataLine;
 
-class Demo {
-    static int i=0;
-    Demo(){
-        System.out.println("i am in con of Demo");
-        i++;
-    }
+class Demo {//start of the class
+    static int i=0; //using this in a static method so it shold be in static
+    Demo(){//constructor of same class
+        System.out.println("i am in con of Demo");//promt
+        i++;//to know that how many time does the constructor invoes
+    }//end of constructor
     
-    public static void main(String[] args) {
-        Demo x=new Demo();
-        System.out.println("i am in main");
-        System.out.println(i);
-        Demo.main(args);
-           
-    }
-}
+    public static void main(String[] args) {//start of main method
+        Demo x=new Demo();// creating an obj of same class
+        System.out.println("i am in main");//promt
+        System.out.println(i);//display intial value in i
+        Demo.main(args);//invoking the same method
+        /*infinty loop here!!!
+        try to stop the excusion by (ctrl+c) on cmd
+        */
+    }//end of main method
+};//end of the class
