81a11bda4d4d8a10fa6da8d23c77e28092c98ac1
21bq1a4210 <113082987+21bq1a4210@users.noreply.github.com>
2022-09-21 21:10:51 +0530

    Update exp-2.java
    added comments

--- SimpleInterace/exp-2.java
+++ SimpleInterace/exp-2.java
@@ -1,31 +1,31 @@
-interface InterfaceDemo{
+interface InterfaceDemo{//start of interface
     /*Interface(){
-        System.out.println("real me in interface");
+        System.out.println("real me in interface"); //the interface doesnt contains constructor
     }*/
     //error: <identifier> expected
     //Interface(){
-    void method1();
-    void method2();
+    void method1();//behivour less methods
+    void method2();//behivour less methods
 }
 
-class IFDemo implements InterfaceDemo{
-    IFDemo(){
-        System.out.println("David:i am in con of implements class\nwhere are u Thony nd jack");
-    }
-    public void method1(){
-        System.out.println("Thony:yo i m in method1");
-    }
-    public void method2(){
-        System.out.println("jack:okay i am in method2");
-    }
-}
+class IFDemo implements InterfaceDemo{//start of implements class
+    IFDemo(){// constructor of the class
+        System.out.println("David:i am in con of implements class\nwhere are u Thony nd jack");//promt
+    }//end of the constructor
+    public void method1(){//start of method1
+        System.out.println("Thony:yo i m in method1");//promt
+    }//end of the method1
+    public void method2(){//start of method2
+        System.out.println("jack:okay i am in method2");//promt
+    }//end of method2
+}//end of IFDemo class
 
-class Demo {
-    public static void main(String[] args) {
-        InterfaceDemo x=new IFDemo();
-        x.method1();
-        x.method2();
-        //InterfaceDemo y=new InterfaceDemo();
-    }
+class Demo {//start of Demo class
+    public static void main(String[] args) {//jp exigution start from here
+        InterfaceDemo x=new IFDemo();//creating constructor of InterfaceDemo type with constructor of IFDemo
+        x.method1();//invoking method of InterfaceDemo
+        x.method2();//invoking method of InterfaceDemo
+        //InterfaceDemo y=new InterfaceDemo();//
+    }//end of main method
     
-}
+};//end of the Demo class
