b4897b755197682334a613e2494a20187f944c9d
21bq1a4210 <113082987+21bq1a4210@users.noreply.github.com>
2022-09-23 06:25:43 +0530

    Update exp-5.java
    added comments

--- b=a++ mul ++a/exp-5.java
+++ b=a++ mul ++a/exp-5.java
@@ -1,7 +1,7 @@
-class Demo{
-    public static void main(String[] args) {
+class Demo{//start of class 
+    public static void main(String[] args) {//start of main method
         int a=5;
         int b=a++*++a;
-        System.out.println(b);
-    }
-}
+        System.out.println(b);//35
+    }//end of main method
+};//end of the class
