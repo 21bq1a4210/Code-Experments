bf933d73d60a9ecde8fd26633357de3cfdb4669b
21bq1a4210 <113082987+21bq1a4210@users.noreply.github.com>
2022-09-24 20:32:19 +0530

    Delete exp-4.java
    ntg

--- b=++a mul a++/exp-4.java
+++
@@ -1,7 +0,0 @@
-class Demo{//class of Demo
-    public static void main(String[] args) {//start of main
-        int a=5;
-        int b=++a*a++;//the operation starts from ++a then a++
-        System.out.println(b);//the ans is 36
-    }//end of main method
-};//end of the class
