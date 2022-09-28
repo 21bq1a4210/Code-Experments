bbe25c7743132611f3cb315fdb0c3d4b09193d04
21bq1a4210 <113082987+21bq1a4210@users.noreply.github.com>
2022-09-28 05:56:21 +0530

    Update exp-7.java
    comments are added

--- find main/exp-7.java
+++ find main/exp-7.java
@@ -1,15 +1,14 @@
-class Demo {
-    public static void main(String[] args) {
-        System.out.println("i am in main 1");
-    }
-    public static void main(int[] args) {
-        System.out.println("i am in main 2");
-    }
-    public void main(Demo[] args) {
-        System.out.println("i am in main 3");
-    }
-    public static void main() {
-        System.out.println("i am in main 4");
-        
-    }
+class Demo {//start of the class 
+    public static void main(String[] args) {// start of correct main method
+        System.out.println("i am in main 1"); //prompt 
+    }//end of main method
+    public static void main(int[] args) {//start of another main method let say 1
+        System.out.println("i am in main 2");//prompt
+    }//end of main method
+    public void main(Demo[] args) {//start of another main method let say 2
+        System.out.println("i am in main 3");//prompt
+    }//end of main method
+    public static void main() {//start of another main method let say 3
+        System.out.println("i am in main 4");//prompt
+    }//end of main method
 }
