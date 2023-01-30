2f7821a2f0dd41195705bea6f6087056dc8e9152
Bandreddi.SarathChandra <113082987+21bq1a4210@users.noreply.github.com>
2023-01-30 10:01:28 +0530

    Add files via upload

---
+++ r basic/St4ings.R
@@ -0,0 +1,26 @@
+"hello"
+'hello'
+str='hello'
+str
+str='wedvnjar v e
+efcf q34 q3
+wfq3w4fq'
+str
+#If you want the line breaks to be inserted at the same position as in the code, use the cat() function:
+cat(str)
+
+
+#String Length
+nchar(str)
+
+#Check a String
+str <- "Hello World!"
+grepl("H", str)
+grepl("Hello", str)
+grepl("X", str)
+
+#Combine Two Strings
+str1 <- "Hello"
+str2 <- "World"
+paste(str1, str2)
+
