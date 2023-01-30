1c5d8956ec9bb797f3ac9d77ad6a370b5aa029c9
Bandreddi.SarathChandra <113082987+21bq1a4210@users.noreply.github.com>
2023-01-30 10:03:47 +0530

    Add files via upload

---
+++ r basic/EscapeChar.R
@@ -0,0 +1,12 @@
+#An escape character is a backslash \ followed by the character you want to insert.
+
+str <- "We are the so-called \"Vikings\", from the north."
+str
+cat(str)
+
+#Code	Result
+#\\	Backslash
+#\n	New Line
+#\r	Carriage Return
+#\t	Tab
+#\b	Backspace
